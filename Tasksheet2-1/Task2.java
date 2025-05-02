cc57eeaf2b8115e47686f3f0ae7538fa3066d846
CBP25 <46953261+CBP25@users.noreply.github.com>
2025-05-02 07:36:28 +0000

    task2-1-c

--- Tasksheet2-1/Task2.java
+++ Tasksheet2-1/Task2.java
@@ -1,26 +1,9 @@
 public class Task2 {
     public static void main(String[] args) {
-        int[] num = {3, 1, 0, 2};
-        boolean T = true;
-        char[] word = {'H', 'w', 'l', 'd','r',' '};
+        int[] n = {3, 1, 0, 2};
+        char[] w = {'H', 'w', 'l', 'd', 'r', ' '};
         String two = "2.0";
-        String say = "";
-
-        say += word[0];
-        say += num[0];
-        say += num[1];
-        say += num[1];
-        say += num[2];
-        say += word[5];
-        say += word[1];
-        say += num[2];
-        say += word[4];
-        say += num[1];
-        say += word[3];
-        say += word[5];
-        say += two;
-        say += word[5];
-        say += T;
-        System.out.println(say); //H3110 w0r1d 2.0 true;
+        boolean T = true;
+        System.out.println("" + w[0] + n[0] + n[1] + n[1] + n[2] + w[5] + w[1] + n[2] + w[4] + n[1] + w[3] + w[5] + two + w[5] + T);
     }
 }
